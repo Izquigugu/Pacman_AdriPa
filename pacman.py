507af287bb4670cb8d6431516bd43d7c2aaed96b
Adrián Luengo <adrianln2206@gmail.com>
2024-11-27 10:57:11 +0100

    Pacman v.2

--- pacman.py
+++ pacman.py
@@ -1,55 +1,51 @@
 import pyxel
 
-
 class Pacman:
-    def __init__(self, x: int, y: int, image, powered: bool):
+    def __init__(self, x: int, y: int, image: int, powered: bool):
         self.x = x
         self.y = y
         self.alive = True
         self.image = image
         self.powered = powered
         self.velocity = 1
+        self.direction = 0
+        self.animation_frame = 0
+        self.animation_speed = 5
+        self.current_sprite = 0
+        pyxel.init(160, 120)
+        pyxel.load("assets/resources.pyxres")
+        pyxel.run(self.update, self.draw)
 
-    @property
-    def x(self) -> int:
-        return self.__x
-    @x.setter
-    def  x(self, x):
-        self.__x = x
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+        self.move()
 
-    @property
-    def y(self) -> int:
-        return self.__y
-    @y.setter
-    def y(self, y):
-        self.__y = y
-
-    @property
-    def alive(self) -> bool:
-        return self.__alive
-    @alive.setter
-    def alive(self, alive):
-        self.__alive = alive
-    # Crear property de image
-    @property
-    def powered(self) -> bool:
-        return self.__powered
-    @powered.setter
-    def powered(self, powered):
-        self.__powered = powered
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.blt(self.x, self.y, 0, 0, 0, 16, 16)
 
     def move(self):
         if pyxel.btn(pyxel.KEY_W):
             self.y -= self.velocity
+            self.direction = 2
         if pyxel.btn(pyxel.KEY_S):
             self.y += self.velocity
+            self.direction = 3
         if pyxel.btn(pyxel.KEY_A):
             self.x -= self.velocity
+            self.direction = 1
         if pyxel.btn(pyxel.KEY_D):
             self.x += self.velocity
+            self.direction = 0
+
+    def update_animation(self):
+        self.animation_frame = ()
 
 
 
 
+
+pacman = Pacman(50,50, 0, False)
 
 
