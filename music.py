df7a483b925d6b019f1d1cc910ed69e2cdfdaa87
Izquigugu <pablo.gut.izqu@gmail.com>
2024-12-06 12:52:12 +0100

    Música de inicio

--- music.py
+++ music.py
@@ -3,11 +3,11 @@ class PyxelSounds:
     # in frames, 60 frames per second
     HIT_WALL_SOUND_TIMEOUT = 120
     def __init__(self):
-        self.__hit_wall_sound_framestamp = 0
+        self.hit_wall_sound_framestamp = 0
         pyxel.playm(0, loop=True)  # Loop music by default
     def play_hit_wall_sound(self):
         current_frame = pyxel.frame_count
-        if current_frame - self.__hit_wall_sound_framestamp < self.HIT_WALL_SOUND_TIMEOUT:
+        if current_frame - self.hit_wall_sound_framestamp < self.HIT_WALL_SOUND_TIMEOUT:
             return
         self.__hit_wall_sound_framestamp = current_frame
         pyxel.play(0, 0)  # Play a sound effect (channel 0, sound 0)
